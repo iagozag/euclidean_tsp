1c7e4cddbbad3d1c332c111ac0af97e31936d626
iagozag <iagozag@gmail.com>
2025-01-06 13:44:02 -0300

    dist2d function added

---
+++ include/Utils.h
@@ -0,0 +1,8 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <cmath>
+
+int dist2d(long double x1, long double y1, long double x2, long double y2);
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#include "Utils.h"
+using namespace std;
+
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+#define endl '\n'
+#define int ll
+
+typedef long long ll;
+
+const int INF = 0x3f3f3f3f3f3f3f3fll;
+
+int32_t main(){ _
+
+
+    exit(0);
+}
