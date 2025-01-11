1fc88ec880af9c8c3d10cb8c987bd1451630b86f
iagozag <iagozag@gmail.com>
2025-01-11 00:49:31 -0300

    Remove lemon-1.3.1/build/ from repository and update .gitignore

--- lemon-1.3.1/build/contrib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ventoy/documents/school/ufmg/programmingSubjects/ALG2/euclidean_tsp/lemon-1.3.1/contrib
-# Build directory: /home/ventoy/documents/school/ufmg/programmingSubjects/ALG2/euclidean_tsp/lemon-1.3.1/build/contrib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
